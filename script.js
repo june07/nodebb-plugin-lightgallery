18e9381430681652e8ad632df24d2c05491153a4
Ubuntu <ubuntu@docker.lxd>
2019-08-02 23:18:03 +0000

    Replaced console with debug.  Removed problematic function.  Added client side detection of topic change.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 require(['lightgallery'], function(lightgallery) {
   require(['lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function(lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
     $(document).ready(function() {
+      initLightGallery();
+    });
+    $(window).on('action:topic.loaded', function(data) {
+      initLightGallery();
+    });
+    function initLightGallery() {
       $("[id^=lightgallery]").lightGallery({
         thumbnail: true,
         selector: 'a'
-      });    
-    });
+      });
+    }
   });
 });
+

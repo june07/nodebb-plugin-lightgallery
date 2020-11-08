ec407ae3845dbe20fdf856053971bf55c87b5b68
june07 <667@onezerohosting.com>
2020-11-08 04:16:13 +0000

    .

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-require(['lightgallery'], function(lightgallery) {
-  require(['jquery', 'lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function($, lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
-    $(document).ready(function() {
-      initLightGallery();
+require(['jquery'], function($) {
+    require(['lightgallery'], function(lightgallery) {
+        require([lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function(lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
+            $(document).ready(function() {
+            initLightGallery();
+            });
+            $(window).on('action:topic.loaded', function(data) {
+            initLightGallery();
+            });
+            function initLightGallery() {
+            $("[id^=lightgallery]").lightGallery({
+                exThumbImage: 'data-exthumbimage',
+                selector: 'a > img'
+            });
+            }
+        });
     });
-    $(window).on('action:topic.loaded', function(data) {
-      initLightGallery();
-    });
-    function initLightGallery() {
-      $("[id^=lightgallery]").lightGallery({
-        exThumbImage: 'data-exthumbimage',
-        selector: 'a > img'
-      });
-    }
-  });
 });
-

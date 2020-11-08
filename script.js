2482d890399c636c78066cabbb0108c39e080be1
june07 <667@onezerohosting.com>
2020-11-08 04:40:08 +0000

    bumped version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-require(['jquery'], function($) {
+require(['jquery'], function(jquery) {
     require(['lightgallery'], function(lightgallery) {
         require([lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function(lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
             $(document).ready(function() {

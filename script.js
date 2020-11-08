01984cd64da9a8dda3a6a95b3ed8d5636b22bcd6
june07 <667@onezerohosting.com>
2020-11-08 19:32:16 +0000

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-require(['jquery'], function(jquery) {
+require(['jquery'], function($) {
     require(['lightgallery'], function(lightgallery) {
-        require([lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function(lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
+        require(['lg-thumbnail', 'lg-autoplay', 'lg-video', 'lg-fullscreen', 'lg-pager', 'lg-zoom', 'lg-hash', 'lg-share'], function(lgthumbnail, lgautoplay, lgvideo, lgfullscreen, lgpager, lgzoom, lghash, lgshare) {
             $(document).ready(function() {
             initLightGallery();
             });
